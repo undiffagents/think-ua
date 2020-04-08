4d24b80f3e3017029179f6d4f6abf9d4deefe0ee
Dario Salvucci <233049+salvucci@users.noreply.github.com>
2020-04-08 11:18:43 -0400

    Removed drs and ace (for now)

--- run/pvt_ace.py
+++
@@ -1,8 +0,0 @@
-from tasks.pvt import ACE_INSTRUCTIONS, PVTTask
-from think import World
-from ua import ACEUndifferentiatedAgent
-
-if __name__ == "__main__":
-    agent = ACEUndifferentiatedAgent()
-    task = PVTTask(agent, instructions=ACE_INSTRUCTIONS)
-    World(task, agent).run(100)
